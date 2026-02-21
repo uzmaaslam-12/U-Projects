8b6f4911b6270a435c885d47677d439e58c30524
uzmaaslam-12 <uzmaaslam289@gmail.com>
2026-02-21 16:37:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-function checkAttendance() {
-  let total = Number(document.getElementById("totalClasses").value);
-  let attended = Number(document.getElementById("attendedClasses").value);
-  let result = document.getElementById("output");
+function checkString() {
+  let input = document.getElementById("userInput").value;
 
-  if (isNaN(total) || isNaN(attended) || total <= 0 || attended < 0) {
-    result.innerHTML = "All fields are required and must be a valid number";
-    return;
-  }
-  if (attended > total) {
-    result.innerHTML = "Attended classes cannot be greater than total number of classes";
+  if (input === "") {
+    alert("Please enter a string");
     return;
   }
 
-  let percentage = (attended / total) * 100;
+  let length = input.length;
+  let upper = input.toUpperCase();
+  let lower = input.toLowerCase();
+  let capitalize = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  let firstChar = input.charAt(0);
+  let lastChar = input.charAt(input.length - 1);
+  let reverse = input.split("").reverse().join("");
 
-  if (percentage >= 75) {
-    resultText = `Attendance: ${percentage.toFixed(2)}% <br>
-    <p class="text-success">✔️ You are eligible!</p>`;
-  } else {
-    resultText = `Attendance: ${percentage.toFixed(2)}% <br>
-    <p class="text-danger">❌ You are not eligible!</p>`;
+  let alphabets = 0;
+
+  for (let i = 0; i < input.length; i++) {
+      let ch = input[i];
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+          alphabets++;
+      }
   }
 
-  document.getElementById("modalResult").innerHTML = resultText;
-  let modal = new bootstrap.Modal(document.getElementById("resultModal"));
-  modal.show();
+  let resultDiv = document.getElementById("result");
+
+  resultDiv.innerHTML = `
+        <p><b>Length:</b> ${length}</p>
+        <p><b>Uppercase:</b> ${upper}</p>
+        <p><b>Lowercase:</b> ${lower}</p>
+        <p><b>Capitalized:</b> ${capitalize}</p>
+        <p><b>First Character:</b> ${firstChar}</p>
+        <p><b>Last Character:</b> ${lastChar}</p>
+        <p><b>Reversed String:</b> ${reverse}</p>
+        <p><b>Number of Characters:</b> ${alphabets}</p>
+    `;
 }
